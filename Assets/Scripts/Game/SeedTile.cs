bc37ff7d2768ab901d935027ef881a31eb4debde
jslee <102969527+eggmong@users.noreply.github.com>
2023-08-21 18:14:52 +0900

    Fade 타입 구현 진행중

--- Assets/Scripts/Game/SeedTile.cs
+++ Assets/Scripts/Game/SeedTile.cs
@@ -1,15 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
-
-public enum SeedType
-{
-    Normal = 0,
-    Heart
-}
-
+using UniRx;
+using System;
+using DG.Tweening;
 
 public class SeedTile : TileBase
 {
@@ -19,6 +13,9 @@ public class SeedTile : TileBase
         get => this.isTouch;
         set => this.isTouch = value;
     }
+
+    private readonly Subject<Unit> actionSubject = new Subject<Unit>();
+    private bool isActionRunning = false;
 
 
 
@@ -32,6 +29,44 @@ public class SeedTile : TileBase
         if (sprite != null)
         {
             this.spriteRenderer.sprite = sprite;
+        }
+
+        switch(this.info.SubType)
+        {
+            case "Fade":
+                {
+                    StartFadeAction();
+                }
+                break;
+        }
+    }
+
+    private void StartFadeAction()
+    {
+        // Fade 타입의 동작을 시작
+        if (isActionRunning == false)
+        {
+            isActionRunning = true;
+            Observable.Start(() =>
+            {
+                // Fade 타입의 동작 코드
+                Debug.Log("Fade 타입 동작 시작");
+                // 여기에서 필요한 동작 수행
+
+                var sequence = DOTween.Sequence()
+            .Append(spriteRenderer.DOFade(0f, 0.5f)) // FadeOut
+            .AppendCallback(() => this.tileCollider.enabled = false) // Collider 비활성화
+            .AppendInterval(0.5f) // 0.5초 대기
+            .Append(spriteRenderer.DOFade(1f, 0.5f)) // FadeIn
+            .AppendCallback(() => this.tileCollider.enabled = true) // Collider 활성화
+            .AppendInterval(0.5f) // 0.5초 대기
+            .SetLoops(-1); // 무한 루프
+
+            }).DoOnCompleted(() =>
+            {
+                isActionRunning = false;
+                actionSubject.OnCompleted(); // 동작이 끝나면 이벤트를 완료
+            }).Subscribe();
         }
     }
 
